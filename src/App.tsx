59046607bd03e6d34d9365fd1fa310e85f51de7e
HelloPipedream <hello@pipedream.digital>
2026-04-29 23:31:43 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -861,7 +861,7 @@ export default function App() {
                       filter: "drop-shadow(0 0 50px rgba(255,255,255,0.4))",
                       y: -10
                     }}
-                    src="CF.png", 
+                    src="CF.png" 
                     alt="AGP4 Commercial" 
                     className="relative z-10 max-h-[380px] w-auto drop-shadow-[0_45px_50px_rgba(0,0,0,0.8)] transition-all duration-700 cursor-pointer" 
                     referrerPolicy="no-referrer"
@@ -917,7 +917,7 @@ export default function App() {
                       filter: "drop-shadow(0 0 40px rgba(43,99,52,0.3))",
                       y: -10
                     }}
-                    src="HG.png", 
+                    src="HG.png"
                     alt="AGP4 Home" 
                     className="relative z-10 max-h-[380px] w-auto drop-shadow-[0_40px_50px_rgba(0,0,0,0.2)] transition-all duration-700 cursor-pointer" 
                     referrerPolicy="no-referrer"
